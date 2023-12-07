d98bfe6be0362b190edfe1c2a5061a9ab6d82671
happyness980817 <happyness980817@gmail.com>
2023-12-07 19:55:02 +0900

    learned useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("rendered");
+  console.log("i run all the time"); // state 변할 때마다 실행
+  useEffect(() => {
+    console.log("CALL THE API...");
+  },[]);
+/*  const iRunOnlyOnce = () => {
+    console.log("i run only once.");
+  };
+  useEffect(iRunOnlyOnce, []); 
+  
+  얘랑 같은 코드*/
   return (
     <div>
       <h1>{counter}</h1>
