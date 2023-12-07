f6a9dd6e4afad3f6f917821b3cfdc9aa2c45121e
happyness980817 <happyness980817@gmail.com>
2023-12-07 20:55:14 +0900

    useEffect argument

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time"); // state 변할 때마다 실행
   useEffect(() => {
     console.log("CALL THE API...");
-  },[]);
+  },[]); // 코드 한번만 실행
+  useEffect(() => {
+    if(keyword !== "") {
+      console.log("SEARCH FOR", keyword); // keyword 가 빈 값이 아닐때 keyword 검색
+    }
+  }, [keyword]); // keyword 가 변화할 때만 code 실행
+
 /*  const iRunOnlyOnce = () => {
     console.log("i run only once.");
   };
@@ -15,6 +23,7 @@ function App() {
   얘랑 같은 코드*/
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
