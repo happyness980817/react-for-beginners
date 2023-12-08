a8a484633806eb7ff9cbe5280c5caf6b416ccf03
happyness980817 <happyness980817@gmail.com>
2023-12-08 11:59:10 +0900

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time"); // state 변할 때마다 실행
-  useEffect(() => {
-    console.log("CALL THE API...");
-  },[]); // 코드 한번만 실행
-  useEffect(() => {
-    if(keyword !== "") {
-      console.log("SEARCH FOR", keyword); // keyword 가 빈 값이 아닐때 keyword 검색
-    }
-  }, [keyword]); // keyword 가 변화할 때만 code 실행
+function Hello() {
+    useEffect(()=>{
+      console.log("hi");
+      return () => console.log("bye")
+    }, []);
+/*     useEffect(function () {
+      console.log("hi");
+      return function(){
+        console.log("bye");
+      }
+    }, []); */
+  return <h1>Hello</h1>;
+}
 
-/*  const iRunOnlyOnce = () => {
-    console.log("i run only once.");
-  };
-  useEffect(iRunOnlyOnce, []); 
-  
-  얘랑 같은 코드*/
+function App() {
+  const [showing,setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
