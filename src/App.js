e542988f8e45bdb3cfcadd1412b050a819aed243
happyness980817 <happyness980817@gmail.com>
2023-12-14 10:00:37 +0900

    movie app part one

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-
   const getMovies = async () => {
-    const response = await fetch(
-      `https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=231083e38b78a31129aa0ad39cf290bd&targetDt=20231212`
-    );
-    const json = await response.json();
-    const boxOfficeList = json.boxOfficeResult.dailyBoxOfficeList;
-    setMovies(boxOfficeList);
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json`
+      )
+    ).json();
+    setMovies(json.data.movies);
     setLoading(false);
   };
-
   useEffect(() => {
     getMovies();
   }, []);
-
   return (
     <div>
-      <h1>My Movie App</h1>
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <ul>
-          {movies.map((movie, index) => (
-            <div key={index}>
-              <h2>{movie.movieNm}</h2>
-              <h3>release date: {movie.openDt}</h3>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
             </div>
           ))}
-        </ul>
+        </div>
       )}
     </div>
   );
 }
-
 export default App;
