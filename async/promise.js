49350c2be537714739d41a50ac34d68ac77cb96d
happyness980817 <happyness980817@gmail.com>
2023-12-21 15:11:32 +0900

    Learned JavaScript Promise

---
+++ async/promise.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// 1. Producer
+const promise = new Promise((resolve,reject) => {
+    console.log('doing something...');
+    setTimeout(() => {
+        resolve('ellie');
+        // reject(new Error('no network'));
+    },2000);
+})
+
+// 2. Consumers: then, catch, finally
+promise
+    .then((value) => {
+        console.log(value);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    .finally(() => {
+        console.log('finally');
+    });
+
+// 3. Promise chaining
+
+const fetchNumber = new Promise((resolve,reject) => {
+    setTimeout(() => resolve(1), 1000);
+});
+
+fetchNumber
+.then(num => num*2)
+.then(num => num*3)
+.then(num => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => resolve(num-1),1000);
+    });
+})
+.then(num => console.log(num));
+
+
+
+
+
