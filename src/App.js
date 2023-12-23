9f6cd538a8e08b2c73421fdd7b2aabef5a9afccb
happyness980817 <happyness980817@gmail.com>
2023-12-23 13:22:43 +0900

    movie app part two (propTypes)

--- src/App.js
+++ src/App.js
@@ -1,37 +1,5 @@
-import { useEffect, useState } from "react";
-import Movie from "./Movie";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async() => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json`
-    );
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies()
-  },[]);
-
-  return (
-  <div>
-    {loading ? <h1>Loading...</h1> : 
-    (<div>
-      {movies.map((movie) => (
-        <Movie 
-          key={movie.id}
-          coverImg={movie.medium_cover_image} 
-          title={movie.title} 
-          summary={movie.summary} 
-          genres={movie.genres}
-        />
-      ))}
-    </div>
-    )}
-  </div>);
+  return null;
 }
 // map 사용 시 element 에 key 부여
 export default App;
