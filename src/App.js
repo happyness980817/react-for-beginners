d34e5308741dbd56cb98034775eb3c63a82cecc7
happyness980817 <happyness980817@gmail.com>
2023-12-23 14:04:29 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 function App() {
-  return null;
+  return (<Router>
+    <Routes>
+      <Route Route path="/movie" element={<Detail />}> 
+      </Route>
+      <Route path="/" element={<Home />}> 
+      </Route>
+    </Routes>
+  </Router>);
 }
 // map 사용 시 element 에 key 부여
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,10 @@
 import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 
 function Movie({coverImg, title, summary, genres}) {
     return (<div>
         <img src={coverImg} alt={title}/>
-        <h2>{title}</h2>
+        <h2><Link to="/movie">{title}</Link></h2>
         <p>{summary}</p>
         <ul>
             {genres.map && genres.map((g) => (<li key={g}>{g}</li>))} 
